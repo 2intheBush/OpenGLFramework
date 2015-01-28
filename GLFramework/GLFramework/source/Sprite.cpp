5e8862842decdf7a053b4d7297aa8343aac97b22
2intheBush <adrianocaple@gmail.com>
2015-01-27 18:00:13 -0800

    sprite constructor for text

--- GLFramework/GLFramework/source/Sprite.cpp
+++ GLFramework/GLFramework/source/Sprite.cpp
@@ -41,6 +41,34 @@ Sprite::Sprite(const char* o_fileName, float o_loc[2], int o_size[2], std::vecto
 	glGenBuffers(1, &uiIBO);
 }
 
+Sprite::Sprite(const char* oFileName, unsigned int location[2], unsigned int size[2])
+{
+	int imageWidth, imageHeight;
+	spriteID = loadTexture(oFileName, imageWidth, imageHeight, bpp);
+	unsigned int bpp = 4;
+	vertices[0].position[0];
+	vertices[0].position[1];
+
+	vertices[1].position[0];
+	vertices[1].position[1];
+	
+	vertices[2].position[0];
+	vertices[2].position[1];
+
+	vertices[3].position[0];
+	vertices[3].position[1];
+
+	for (int i = 0; i < 4; i++)
+	{
+		vertices[i].position[2] = 0.0f;
+		vertices[i].position[3] = 1.0f;
+		vertices[i].color[0] = 1.f;
+		vertices[i].color[1] = 1.f;
+		vertices[i].color[2] = 1.f;
+		vertices[i].color[3] = 1.f;
+	}
+}
+
 unsigned int Sprite::loadTexture(const char* o_fileName, int & a_iWidth, int & a_iHeight, int & a_iBPP)
 {
 	unsigned int uiTextureID = 0;
